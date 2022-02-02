d038e5c77b2dc5bf79cffb27e59e2550d6f33267
joxen90 <98822495+joxen90@users.noreply.github.com>
2022-02-02 15:51:03 +0100

    Update KKN.py

--- KKN.py
+++ KKN.py
@@ -16,3 +16,14 @@ print(df['custcat'].value_counts())
 df.hist(column='income', bins=50)
 #plt.hist(n, 50, facecolor='blue', alpha=0.5)
 plt.show()
+
+print(df.columns)
+
+X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values  #.astype(float)
+print( X[0:5] )
+
+y = df['custcat'].values
+print( y[0:5] )
+
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+print ( X[0:5])
