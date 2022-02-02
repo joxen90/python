054b1f5ade85855218ef17c063661dd70079b03b
joxen90 <ericson.joakim@gmail.com>
2022-02-02 21:20:08 +0100

    Create LogisticRegression.py

---
+++ LogisticRegression.py
@@ -0,0 +1,19 @@
+print("Logistic regression")
+
+import pandas as pd
+import pylab as pl
+import numpy as np
+import scipy.optimize as opt
+from sklearn import preprocessing
+#%matplotlib inline 
+import matplotlib.pyplot as plt
+
+!wget -O ChurnData.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv
+
+churn_df = pd.read_csv("ChurnData.csv")
+churn_df.head()
+
+churn_df = churn_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip',   'callcard', 'wireless','churn']]
+churn_df['churn'] = churn_df['churn'].astype('int')
+churn_df.head()
+churn_df.shape
