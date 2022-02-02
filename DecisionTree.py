0b126962728472c755a0d8a8cc5332bfc77101db
joxen90 <ericson.joakim@gmail.com>
2022-02-02 20:01:36 +0100

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -11,4 +11,25 @@ from sklearn.tree import DecisionTreeClassifier
 my_data = pd.read_csv("drug200.csv", delimiter=",")
 my_data[0:5]
 
-my_data.shape
+print(my_data.shape)
+
+X = my_data[['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']].values
+X[0:5]
+
+from sklearn import preprocessing
+le_sex = preprocessing.LabelEncoder()
+le_sex.fit(['F','M'])
+X[:,1] = le_sex.transform(X[:,1]) 
+
+print(X[:,1])
+
+le_BP = preprocessing.LabelEncoder()
+le_BP.fit([ 'LOW', 'NORMAL', 'HIGH'])
+X[:,2] = le_BP.transform(X[:,2])
+
+print(X)
+le_Chol = preprocessing.LabelEncoder()
+le_Chol.fit([ 'NORMAL', 'HIGH'])
+X[:,3] = le_Chol.transform(X[:,3]) 
+
+X[0:5]
