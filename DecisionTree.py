7cf71d1653e1a86de0c4e4ec580b24771da5633c
joxen90 <ericson.joakim@gmail.com>
2022-02-02 19:56:38 +0100

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,2 +1,12 @@
 
 print("DecisionTree")
+
+import numpy as np 
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+wget -O drug200.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/drug200.csv
+
+
+my_data = pd.read_csv("drug200.csv", delimiter=",")
+my_data[0:5]
