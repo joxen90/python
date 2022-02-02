f36bfdf34f32a15d068f8f506263681ba908b234
joxen90 <98822495+joxen90@users.noreply.github.com>
2022-02-02 15:06:36 +0100

    Update KKN.py

--- KKN.py
+++ KKN.py
@@ -3,4 +3,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
-%matplotlib inline
+#matplotlib inline
+
+!wget -O teleCust1000t.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv
