6fc3a907e02699a797d2717f445a08a381fcb1ec
joxen90 <ericson.joakim@gmail.com>
2022-02-02 15:56:55 +0100

    Update KKN.py

--- KKN.py
+++ KKN.py
@@ -29,3 +29,22 @@ X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
 print ( X[0:5])
 
 print("windows git")
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+k = 4
+#Train Model and Predict  
+neigh = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
+print(neigh)
+
+yhat = neigh.predict(X_test)
+yhat[0:5]
+
+from sklearn import metrics
+print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
+print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
