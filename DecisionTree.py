5aba43aad722bdb5f7b6b3e2ffc8cee320e234df
joxen90 <ericson.joakim@gmail.com>
2022-02-02 21:06:01 +0100

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -43,3 +43,33 @@ X_trainset, X_testset, y_trainset, y_testset = train_test_split(X, y, test_size=
 
 print("X trainset", X_trainset.shape)
 print("Y trainset", y_trainset.shape)
+
+drugTree = DecisionTreeClassifier(criterion="entropy", max_depth = 4)
+drugTree # it shows the default parameters
+
+drugTree.fit(X_trainset,y_trainset)
+
+predTree = drugTree.predict(X_testset)
+
+print (predTree [0:5])
+print (y_testset [0:5])
+
+from sklearn import metrics
+import matplotlib.pyplot as plt
+print("DecisionTrees's Accuracy: ", metrics.accuracy_score(y_testset, predTree))
+
+from  io import StringIO
+import pydotplus
+import matplotlib.image as mpimg
+from sklearn import tree
+%matplotlib inline 
+
+dot_data = StringIO()
+filename = "drugtree.png"
+featureNames = my_data.columns[0:5]
+out=tree.export_graphviz(drugTree,feature_names=featureNames, out_file=dot_data, class_names= np.unique(y_trainset), filled=True,  special_characters=True,rotate=False)  
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_png(filename)
+img = mpimg.imread(filename)
+plt.figure(figsize=(100, 200))
+plt.imshow(img,interpolation='nearest')
