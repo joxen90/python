52f4a82f101f08848588357eddcd197a5ee4862e
joxen90 <98822495+joxen90@users.noreply.github.com>
2022-02-02 15:27:28 +0100

    Update KKN.py

--- KKN.py
+++ KKN.py
@@ -5,5 +5,9 @@ import numpy as np
 from sklearn import preprocessing
 #matplotlib inline
 
-wget -O teleCust1000t.csv 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv'
-#wget -O teleCust1000t.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv
+!wget -O teleCust1000t.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv
+
+df = pd.read_csv('teleCust1000t.csv')
+df.head()
+
+df['custcat'].value_counts()
