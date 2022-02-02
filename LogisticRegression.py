0b60a56e53506d3a89d1c640d9d0c52c46b5e6be
joxen90 <ericson.joakim@gmail.com>
2022-02-02 21:21:47 +0100

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -8,7 +8,7 @@ from sklearn import preprocessing
 #%matplotlib inline 
 import matplotlib.pyplot as plt
 
-!wget -O ChurnData.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv
+#!wget -O ChurnData.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv
 
 churn_df = pd.read_csv("ChurnData.csv")
 churn_df.head()
