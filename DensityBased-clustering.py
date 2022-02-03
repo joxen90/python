33e1f4e715fa37f58c47e50f949fa25926159933
joxen90 <ericson.joakim@gmail.com>
2022-02-03 14:24:29 +0100

    Create DensityBased-clustering.py

---
+++ DensityBased-clustering.py
@@ -0,0 +1,26 @@
+import numpy as np 
+from sklearn.cluster import DBSCAN 
+from sklearn.datasets.samples_generator import make_blobs 
+from sklearn.preprocessing import StandardScaler 
+import matplotlib.pyplot as plt 
+#matplotlib inline
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+def createDataPoints(centroidLocation, numSamples, clusterDeviation):
+    # Create random data and store in feature matrix X and response vector y.
+    X, y = make_blobs(n_samples=numSamples, centers=centroidLocation, 
+                                cluster_std=clusterDeviation)
+    
+    # Standardize features by removing the mean and scaling to unit variance
+    X = StandardScaler().fit_transform(X)
+    return X, y
+
+
+X, y = createDataPoints([[4,3], [2,-1], [-1,4]] , 1500, 0.5)
+
+epsilon = 0.3
+minimumSamples = 7
+db = DBSCAN(eps=epsilon, min_samples=minimumSamples).fit(X)
+labels = db.labels_
+labels
