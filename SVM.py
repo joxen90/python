02a8793f2966021ce923011c5ab5c9b81f9de629
joxen90 <ericson.joakim@gmail.com>
2022-02-03 11:23:23 +0100

    Create SVM.py

---
+++ SVM.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import pylab as pl
+import numpy as np
+import scipy.optimize as opt
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+%matplotlib inline 
+import matplotlib.pyplot as plt
+
+#wget -O cell_samples.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/cell_samples.csv
+
+cell_df = pd.read_csv("cell_samples.csv")
+cell_df.head()
+
+ax = cell_df[cell_df['Class'] == 4][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='DarkBlue', label='malignant');
+cell_df[cell_df['Class'] == 2][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='Yellow', label='benign', ax=ax);
+plt.show()
+
+cell_df.dtypes
