70b9e8295ae880cb4ad3bcdb5204e5e67aa81630
joxen90 <ericson.joakim@gmail.com>
2022-02-03 12:22:35 +0100

    Update k-means.py

--- k-means.py
+++ k-means.py
@@ -8,7 +8,7 @@ from sklearn.datasets import make_blobs
 
 np.random.seed(0)
 
-X, y = make_blobs(n_samples=5000, centers=[[4,4], [-2, -1], [2, -3], [1, 1]], cluster_std=0.9)
+X, y = make_blobs(n_samples=5000, centers=[[4,4], [-2, -1], [2, -3], [1, 1],[1, 5] ], cluster_std=0.9)
 
 plt.scatter(X[:, 0], X[:, 1], marker='.')
 
