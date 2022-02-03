fb1025d7de34048e9ee5bf7162fb41d57bf51a6e
joxen90 <ericson.joakim@gmail.com>
2022-02-03 14:25:00 +0100

    Update DensityBased-clustering.py

--- DensityBased-clustering.py
+++ DensityBased-clustering.py
@@ -1,6 +1,6 @@
 import numpy as np 
 from sklearn.cluster import DBSCAN 
-from sklearn.datasets.samples_generator import make_blobs 
+from sklearn.datasets import make_blobs 
 from sklearn.preprocessing import StandardScaler 
 import matplotlib.pyplot as plt 
 #matplotlib inline
