e86d98a08186360c921b3c396bc1e940a82d95f2
joxen90 <ericson.joakim@gmail.com>
2022-02-03 15:22:54 +0100

    Create ContentBased.py

---
+++ ContentBased.py
@@ -0,0 +1,65 @@
+#!wget -O moviedataset.zip https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%205/data/moviedataset.zip
+print('unziping ...')
+#!unzip -o -j moviedataset.zip 
+
+#Dataframe manipulation library
+import pandas as pd
+#Math functions, we'll only need the sqrt function so let's import only that
+from math import sqrt
+import numpy as np
+import matplotlib.pyplot as plt
+#matplotlib inline
+
+#Storing the movie information into a pandas dataframe
+movies_df = pd.read_csv('movies.csv')
+#Storing the user information into a pandas dataframe
+ratings_df = pd.read_csv('ratings.csv')
+#Head is a function that gets the first N rows of a dataframe. N's default is 5.
+movies_df.head()
+
+#Every genre is separated by a | so we simply have to call the split function on |
+movies_df['genres'] = movies_df.genres.str.split('|')
+movies_df.head()
+
+#Copying the movie dataframe into a new one since we won't need to use the genre information in our first case.
+moviesWithGenres_df = movies_df.copy()
+
+#For every row in the dataframe, iterate through the list of genres and place a 1 into the corresponding column
+for index, row in movies_df.iterrows():
+    for genre in row['genres']:
+        moviesWithGenres_df.at[index, genre] = 1
+#Filling in the NaN values with 0 to show that a movie doesn't have that column's genre
+moviesWithGenres_df = moviesWithGenres_df.fillna(0)
+moviesWithGenres_df.head()
+
+#Using regular expressions to find a year stored between parentheses
+#We specify the parantheses so we don't conflict with movies that have years in their titles
+movies_df['year'] = movies_df.title.str.extract('(\(\d\d\d\d\))',expand=False)
+#Removing the parentheses
+movies_df['year'] = movies_df.year.str.extract('(\d\d\d\d)',expand=False)
+#Removing the years from the 'title' column
+movies_df['title'] = movies_df.title.str.replace('(\(\d\d\d\d\))', '')
+#Applying the strip function to get rid of any ending whitespace characters that may have appeared
+movies_df['title'] = movies_df['title'].apply(lambda x: x.strip())
+movies_df.head()
+
+#Every genre is separated by a | so we simply have to call the split function on |
+movies_df['genres'] = movies_df.genres.str.split('|')
+movies_df.head()
+
+#Copying the movie dataframe into a new one since we won't need to use the genre information in our first case.
+moviesWithGenres_df = movies_df.copy()
+
+#For every row in the dataframe, iterate through the list of genres and place a 1 into the corresponding column
+for index, row in movies_df.iterrows():
+    for genre in row['genres']:
+        moviesWithGenres_df.at[index, genre] = 1
+#Filling in the NaN values with 0 to show that a movie doesn't have that column's genre
+moviesWithGenres_df = moviesWithGenres_df.fillna(0)
+moviesWithGenres_df.head()
+
+ratings_df.head()
+
+#Drop removes a specified row or column from a dataframe
+ratings_df = ratings_df.drop('timestamp', 1)
+ratings_df.head()
