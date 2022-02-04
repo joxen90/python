9accdbfa7a77d48a1dbc05f812c00de7fae12838
joxen <ericson.joakim@gmail.com>
2022-02-04 14:25:44 +0100

    kommenterad kod

--- DecisionTree.py
+++ DecisionTree.py
@@ -14,28 +14,28 @@ my_data[0:5]
 print(my_data.shape)
 
 X = my_data[['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']].values
-X[0:5]
+print(X[0:5])
 
 from sklearn import preprocessing
 le_sex = preprocessing.LabelEncoder()
 le_sex.fit(['F','M'])
 X[:,1] = le_sex.transform(X[:,1]) 
 
-#print(X[:,1])
+print(X[0:5])
 
 le_BP = preprocessing.LabelEncoder()
 le_BP.fit([ 'LOW', 'NORMAL', 'HIGH'])
 X[:,2] = le_BP.transform(X[:,2])
 
-print(X)
+print(X[0:5])
 le_Chol = preprocessing.LabelEncoder()
 le_Chol.fit([ 'NORMAL', 'HIGH'])
 X[:,3] = le_Chol.transform(X[:,3]) 
 
-X[0:5]
+print(X[0:5])
 
 y = my_data["Drug"]
-y[0:5]
+print(y[0:5])
 
 from sklearn.model_selection import train_test_split
 
@@ -44,8 +44,9 @@ X_trainset, X_testset, y_trainset, y_testset = train_test_split(X, y, test_size=
 print("X trainset", X_trainset.shape)
 print("Y trainset", y_trainset.shape)
 
-drugTree = DecisionTreeClassifier(criterion="entropy", max_depth = 4)
-drugTree # it shows the default parameters
+drugTree = DecisionTreeClassifier(criterion="entropy", max_depth = 5)
+print("Drugtree")
+print(drugTree ) # it shows the default parameters
 
 drugTree.fit(X_trainset,y_trainset)
 
@@ -73,3 +74,6 @@ graph.write_png(filename)
 img = mpimg.imread(filename)
 plt.figure(figsize=(100, 200))
 plt.imshow(img,interpolation='nearest')
+
+
+
