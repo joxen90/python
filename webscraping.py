643c3417b5634d579545790b43fee264c7ff1d93
joxen90 <ericson.joakim@gmail.com>
2022-03-16 09:29:45 +0100

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -1,9 +1,9 @@
-from bs4 import BeautifulSoup # this module helps in web scrapping.
+from bs4 import BeautifulSoup as bsp4 # this module helps in web scrapping.
 import requests  # this module helps us to download a web page
 
 html="<!DOCTYPE html><html><head><title>Page Title</title></head><body><h3><b id='boldest'>Lebron James</b></h3><p> Salary: $ 92,000,000 </p><h3> Stephen Curry</h3><p> Salary: $85,000, 000 </p><h3> Kevin Durant </h3><p> Salary: $73,200, 000</p></body></html>"
 
 
-soup = BeautifulSoup(html, "html.parser")
+soup =bs4p(html, "html.parser")
 
 print(soup)
