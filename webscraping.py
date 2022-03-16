e3ffe6445d8436969fa105f6314190fd2706fbad
joxen90 <ericson.joakim@gmail.com>
2022-03-16 09:30:16 +0100

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -4,6 +4,6 @@ import requests  # this module helps us to download a web page
 html="<!DOCTYPE html><html><head><title>Page Title</title></head><body><h3><b id='boldest'>Lebron James</b></h3><p> Salary: $ 92,000,000 </p><h3> Stephen Curry</h3><p> Salary: $85,000, 000 </p><h3> Kevin Durant </h3><p> Salary: $73,200, 000</p></body></html>"
 
 
-soup =bs4p(html, "html.parser")
+soup =bsp4(html, "html.parser")
 
 print(soup)
